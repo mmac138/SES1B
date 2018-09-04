4d016ebfcbaba899668754c059ae165eb3feccd0
Cheryl711 <ahorukawa711@163.com>
2018-09-04 20:30:40 +1000

    add the table to the heart rate

--- HealthClientApp-master/SESHealth/app/src/main/java/yourteamnumber/seshealthpatient/Fragments/HeartRateFragment.java
+++ HealthClientApp-master/SESHealth/app/src/main/java/yourteamnumber/seshealthpatient/Fragments/HeartRateFragment.java
@@ -3,10 +3,14 @@ package yourteamnumber.seshealthpatient.Fragments;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.os.Handler;
+import android.os.Message;
+import android.os.PowerManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
@@ -16,6 +20,7 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
@@ -30,7 +35,13 @@ import android.content.res.Configuration;
 import android.hardware.Camera.PreviewCallback;
 import android.widget.Toast;
 
-
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.chart.PointStyle;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -39,6 +50,12 @@ public class HeartRateFragment extends Fragment {
     public HeartRateFragment() {
         // Required empty public constructor
     }
+    private XYSeries series;
+    private XYMultipleSeriesDataset mDataset;
+    private GraphicalView chart;
+    private XYMultipleSeriesRenderer renderer;
+    private static TextView text = null;
+    private static PowerManager.WakeLock wakeLock = null;
 
     private Timer timer = new Timer();
     private TimerTask task;
@@ -61,7 +78,7 @@ public class HeartRateFragment extends Fragment {
     private static SurfaceHolder previewHolder = null;
     private static Camera camera = null;
 
-    private static TextView text = null;
+
     private static Button btnHR = null;
 
 
@@ -70,10 +87,10 @@ public class HeartRateFragment extends Fragment {
     private static final int[] averageArray = new int[averageArraySize];
 
     public static enum TYPE {
-        GREEN, RED
+        BLACK, RED
     };
 
-    private static TYPE currentType = TYPE.GREEN;
+    private static TYPE currentType = TYPE.BLACK;
 
     public static TYPE getCurrent() {
         return currentType;
@@ -93,16 +110,62 @@ public class HeartRateFragment extends Fragment {
         View x = inflater.inflate(R.layout.fragment_heart_rate, container, false);
 
         context = getActivity().getApplicationContext();
-
+//use this method to allow camera work
         if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA)
                 == PackageManager.PERMISSION_GRANTED) {
             Log.i("TEST","Granted");
-            //init(barcodeScannerView, getIntent(), null);
         } else {
             ActivityCompat.requestPermissions(getActivity(),
                     new String[]{Manifest.permission.CAMERA}, 1);//1 can be another integer
         }
+//the table
+        // add the table into this layout
+        LinearLayout layout = (LinearLayout) x.findViewById(R.id.linearLayout1);
 
+        // This class is used to place all points on a curve, a collection of points, and plot the curves based on those points
+        series = new XYSeries(title);
+
+        // Create an instance of a data set that will be used to create a chart
+        mDataset = new XYMultipleSeriesDataset();
+
+        //Add the point set to the data set
+        mDataset.addSeries(series);
+
+        // format
+        int color = Color.BLACK;
+        PointStyle style = PointStyle.CIRCLE;
+        renderer = buildRenderer(color, style, true);
+
+        setChartSettings(renderer, "X", "Y", 0, 300, 4, 16, Color.WHITE,
+                Color.WHITE);
+
+        //generate the chart
+        chart = ChartFactory.getLineChartView(context, mDataset, renderer);
+
+        // add
+        layout.addView(chart, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
+                ViewGroup.LayoutParams.FILL_PARENT));
+
+
+        // The Handler instance here will work with the Timer instance below to update the chart periodically
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                updateChart();
+                super.handleMessage(msg);
+            }
+        };
+
+        task = new TimerTask() {
+            @Override
+            public void run() {
+                Message message = new Message();
+                message.what = 1;
+                handler.sendMessage(message);
+            }
+        };
+// curve
+        timer.schedule(task, 2, 40);
 
         preview = (SurfaceView) x.findViewById(R.id.preview);
         previewHolder = preview.getHolder();
@@ -111,7 +174,105 @@ public class HeartRateFragment extends Fragment {
 
         text = (TextView) x.findViewById(R.id.text);
         return x;
+
     }
+
+    //Turn off the Timer when closing the program
+    @Override
+    public void onDestroy() {
+        timer.cancel();
+        super.onDestroy();
+    };
+
+    protected XYMultipleSeriesRenderer buildRenderer(int color,
+                                                     PointStyle style, boolean fill) {
+        XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
+
+        // format
+        XYSeriesRenderer r = new XYSeriesRenderer();
+        r.setColor(Color.RED);
+        r.setLineWidth(1);
+        renderer.addSeriesRenderer(r);
+        return renderer;
+    }
+
+    protected void setChartSettings(XYMultipleSeriesRenderer renderer,
+                                    String xTitle, String yTitle, double xMin, double xMax,
+                                    double yMin, double yMax, int axesColor, int labelsColor) {
+        // API documentation for rendering graphs
+        renderer.setChartTitle(title);
+        renderer.setXTitle(xTitle);
+        renderer.setYTitle(yTitle);
+        renderer.setXAxisMin(xMin);
+        renderer.setXAxisMax(xMax);
+        renderer.setYAxisMin(yMin);
+        renderer.setYAxisMax(yMax);
+        renderer.setAxesColor(axesColor);
+        renderer.setLabelsColor(labelsColor);
+        renderer.setShowGrid(true);
+        renderer.setGridColor(Color.BLACK);
+        renderer.setXLabels(20);
+        renderer.setYLabels(10);
+        renderer.setXTitle("Time");
+        renderer.setYTitle("mmHg");
+        renderer.setYLabelsAlign(Paint.Align.RIGHT);
+        renderer.setPointSize((float) 3);
+        renderer.setShowLegend(false);
+    }
+
+    private void updateChart() {
+
+        // add next note
+        if (flag == 1)
+            addY = 10;
+        else {
+            flag = 1;
+            if (gx < 200) {
+                if (hua[20] > 1) {
+                    hua[20] = 0;
+                }
+                hua[20]++;
+                return;
+            } else
+                hua[20] = 10;
+            j = 0;
+
+        }
+        if (j < 20) {
+            addY = hua[j];
+            j++;
+        }
+
+        // remove the point set
+        mDataset.removeSeries(series);
+
+        // Determine how many points are in the current set, because the screen can only hold a total of 100, so when the number of points exceeds 100, the length is always 100
+        int length = series.getItemCount();
+        int bz = 0;
+        // addX = length;
+        if (length > 300) {
+            length = 300;
+            bz = 1;
+        }
+        addX = length;
+        // The values of x and y in the old point set were taken out and put into backup, and the values of x were added 1, resulting in a shift to the right of the curve
+        for (int i = 0; i < length; i++) {
+            xv[i] = (int) series.getX(i) - bz;
+            yv[i] = (int) series.getY(i);
+        }
+
+        // clear
+        series.clear();
+        mDataset.addSeries(series);
+
+        series.add(addX, addY);
+        for (int k = 0; k < length; k++) {
+            series.add(xv[k], yv[k]);
+        }
+
+        chart.invalidate();
+    }
+
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
@@ -177,7 +338,7 @@ public class HeartRateFragment extends Fragment {
                     flag = 0;
                 }
             } else if (imgAvg > rollingAverage) {
-                newType = TYPE.GREEN;
+                newType = TYPE.BLACK;
             }
 
             if (averageIndex == averageArraySize)
